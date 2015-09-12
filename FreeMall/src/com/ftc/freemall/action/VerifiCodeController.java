f20b05f0ef101c9e0bf08f0dc7897ce2f0b0429d
kunfang <281130208@qq.com>
2015-09-12 15:04:06 +0800

    短线发送--云通讯

--- FreeMall/src/com/ftc/freemall/action/VerifiCodeController.java
+++ FreeMall/src/com/ftc/freemall/action/VerifiCodeController.java
@@ -3,9 +3,11 @@ package com.ftc.freemall.action;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Set;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,42 +35,33 @@ public class VerifiCodeController {
 	}
 
 	@RequestMapping(params="method=sendCode")
-	public String sendVerifiCode(@RequestParam(value="receiverMobile",required=true) String receiverMobile,Model model,HttpServletResponse response){
+	public void sendVerifiCode(@RequestParam(value="receiverMobile",required=true) String receiverMobile,Model model,HttpServletResponse response, HttpSession session){
 		CCPRestSmsSDK sendAPI = VerifiCode.getInstance();
 		
 		String verifiCode = VerifiCode.getVerifiCode();
 		
-		//HashMap<String, Object> result = sendAPI.sendTemplateSMS(receiverMobile,DefaultParams.findParam("verificode.modleid") ,new String[]{verifiCode,DefaultParams.findParam("verificode.timelimit")});
+		HashMap<String, Object> result = sendAPI.sendTemplateSMS(receiverMobile,DefaultParams.findParam("verificode.modleid") ,new String[]{verifiCode,DefaultParams.findParam("verificode.timelimit")});
 		
-		String statusCode = "000000";//(String)result.get("statusCode");
+		String statusCode = (String)result.get("statusCode");
 		
-		/*if("000000".equals(result.get("statusCode"))){
-			//正常返回输出data包体信息（map）
-			HashMap<String,Object> data = (HashMap<String, Object>) result.get("data");
-			Set<String> keySet = data.keySet();
-			for(String key:keySet){
-				Object object = data.get(key);
-				System.out.println(key +" = "+object);
-			}
-		}else{
+		if(!"000000".equals(statusCode)){
 			//异常返回输出错误码和错误信息
 			System.out.println("错误码=" + result.get("statusCode") +" 错误信息= "+result.get("statusMsg"));
-		}*/
+		}
 		
 		PrintWriter write = null;
 		try {
-			if ("000000".equals(statusCode)) {
+			/*if ("000000".equals(statusCode)) {
 				VerifiCodeVO objVO = new VerifiCodeVO();
 				objVO.setPhone(receiverMobile);
 				objVO.setVerificode(verifiCode);
 				verifiCodeService.insertVerifiCode(objVO);
-			}
+			}*/
 			write = response.getWriter();
-			write.print(statusCode);
+			write.print(statusCode+"|"+verifiCode);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return null;
 	}
 	
 	public String checkVerifiCode() {
